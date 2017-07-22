7fbdb19bb30f346b9c5fe696fa7bae0f6ab3acaa
Donald Oakes <donald@oakesville.io>
2017-07-22 11:07:53 -0600

    Handle malformed JSON.

--- src/io/limberest/json/JsonRestService.java
+++ src/io/limberest/json/JsonRestService.java
@@ -3,17 +3,25 @@ package io.limberest.json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import io.limberest.service.ServiceException;
 import io.limberest.service.http.RestService;
+import io.limberest.service.http.Status;
 
 @Consumes("application/json")
 @Produces("application/json")
 public abstract class JsonRestService extends RestService<JSONObject> {
 
     @Override
-    public JSONObject getBody(String text) {
-        return new JsonObject(text);
+    public JSONObject getBody(String text) throws ServiceException {
+        try {
+            return new JsonObject(text);
+        }
+        catch (JSONException ex) {
+            throw new ServiceException(Status.BAD_REQUEST, ex.getMessage());
+        }
     }
 
     @Override
