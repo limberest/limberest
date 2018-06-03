938f8a644334dfafa6d5f4156bd1e83803ccb940
Donald Oakes <donald@oakesville.io>
2018-06-03 09:06:42 -0600

    1.2.3

--- src/io/limberest/json/JsonObject.java
+++ src/io/limberest/json/JsonObject.java
@@ -1,5 +1,7 @@
 package io.limberest.json;
 
+import java.util.Comparator;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -11,15 +13,25 @@ import org.json.JSONObject;
  * (for either automated or eyeball comparisons).
  */
 public class JsonObject extends JSONObject {
-    
+
     public JsonObject() {
     }
-    
+
     public JsonObject(String source) {
         super(source);
     }
-    
+
     public Set<String> keySet() {
         return new TreeSet<String>(super.keySet());
+    }
+
+    protected Set<Entry<String,Object>> entrySet() {
+        Set<Entry<String,Object>> entries = new TreeSet<>(new Comparator<Entry<String,Object>>() {
+            public int compare(Entry<String,Object> e1, Entry<String,Object> e2) {
+                return e1.getKey().compareTo(e2.getKey());
+            }
+        });
+        entries.addAll(super.entrySet());
+        return entries;
     }
 }
